0b5d079316d1827b668771a03f87ce3cbc10adb9
mateoo04 <144594500+mateoo04@users.noreply.github.com>
2024-07-30 13:48:37 +0200

    Add starting directories and files

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const { watchFile } = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        })
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode:'development',
+    devtool:'inline-source-map',
+    devServer:{
+        static:'./dist',
+        watchFiles:['src/*.*']
+    }
+}
