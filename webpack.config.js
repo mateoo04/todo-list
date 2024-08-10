3b5cce0d4d50362ac74c86b40e0fdc1ed796dbd7
mateoo04 <144594500+mateoo04@users.noreply.github.com>
2024-08-10 15:20:56 +0200

    Implement babel
    Code is now compatible with older browsers.

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,45 @@ const { watchFile } = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { type } = require('os');
 const path = require('path');
- 
+
 module.exports = {
-    entry: './src/index.js',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        })
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2)$/i,
+        type: 'asset/resource',
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test:/\.(png|svg)$/i,
-                type:'asset/resource',
-            },{
-                test: /\.(woff|woff2)$/i,
-                type:'asset/resource',
-            },
-        ],
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-        watchFiles: ['src/*.*']
-    }
-}
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    watchFiles: ['src/*.*'],
+  },
+};
